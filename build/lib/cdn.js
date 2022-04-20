ba3e66ac38800db7dda2f7129cb8218a0ff647ff
P2K0 <3364148324@qq.com>
2022-04-20 23:33:45 +0800

    fix: 低版本浏览器 加载模块 报错

--- build/lib/cdn.js
+++ build/lib/cdn.js
@@ -1,11 +1,6 @@
 module.exports = {
   externalsType: "script",
   externals: {
-    "vue-router": ["https://cdn.bootcdn.net/ajax/libs/vue-router/3.5.1/vue-router.min.js", "VueRouter"],
-    "element-ui": ["https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.1/index.min.js", "ELEMENT"],
-    vue: ["https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js", "Vue"],
-    vuex: ["https://cdn.bootcdn.net/ajax/libs/vuex/3.6.2/vuex.min.js", "Vuex"],
-    axios: ["https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js", "axios"],
-    nprogress: ["https://cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.min.js", "NProgress"]
+    Vue: ["https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.min.js", "vue"]
   }
 };
